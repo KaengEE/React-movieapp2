d05e77a052f26872f525c54cf81b1d87c332c49d
KaengEE <yopuiqwer@naver.com>
2023-11-23 14:24:06 +0900

    네브링크와 액티브클래스 css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import Star from "../../assets/glowing-star.png";
 import Party from "../../assets/partying-face.png";
 
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 //rfc 단축키
 export default function Navbar() {
@@ -16,18 +16,18 @@ export default function Navbar() {
 
       <div className="navbar_links">
         <DarkMode />
-        <Link to="/">
+        <NavLink to="/">
           <img className="navbar_emoji" src={Fire} alt="fire emoji" />
           인기순
-        </Link>
-        <Link to="/top_rated">
+        </NavLink>
+        <NavLink to="/top_rated">
           <img className="navbar_emoji" src={Star} alt="star emoji" />
           평점순
-        </Link>
-        <Link to="/upcoming">
+        </NavLink>
+        <NavLink to="/upcoming">
           <img className="navbar_emoji" src={Party} alt="party emoji" />
           최신순
-        </Link>
+        </NavLink>
       </div>
     </nav>
   );
